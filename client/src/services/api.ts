5e3d105d048358ebfb6d1724c955f126773479b7
Felipe Santana <felipe.jesus0801@gmail.com>
2023-12-15 14:43:51 -0300

    feat: update service api

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://127.0.0.1:3333",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "Content-Type": "application/json"
   }
