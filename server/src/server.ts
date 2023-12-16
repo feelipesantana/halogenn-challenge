41d2c5fa403f19275e6a57b2547d57dda636f7f5
Felipe <felipe@MacBook-Pro-de-Felipe.local>
2023-12-16 09:16:11 -0800

    fix: to port 3333

--- server/src/server.ts
+++ server/src/server.ts
@@ -15,7 +15,7 @@ app.use(cors({
 app.use(bodyParser.json());
 app.use('/', appRoutes);
 app.use('/images', express.static(__dirname + '/images'));
-app.listen(3334, () =>{
+app.listen(3333, () =>{
   console.log('Servidor rodando na porta 3333');
 
 })  
