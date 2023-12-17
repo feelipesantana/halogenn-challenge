bad76a44139a3f973f283b32cf9bf894a88a871e
Felipe Santana <felipe.jesus0801@gmail.com>
2023-12-17 13:50:41 -0300

    refactore: get out redux

--- client/src/providers/index.tsx
+++ client/src/providers/index.tsx
@@ -1,14 +1,11 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode } from "react";
-import { ReduxProvider } from "./ReduxProvider";
 
 export function AppProvider({ children }: { children: ReactNode }) {
   const queryClient = new QueryClient();
 
   return (
-    <ReduxProvider>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </ReduxProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 }
